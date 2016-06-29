20a9e55936e0e43693b2b04a22385335a6bb5324
Andrew de Koning <andrew.dekoning@gmail.com>
2016-06-29 18:47:12 -0400

    Added sword object
    - Creates a sword object and applies it to the character class in an
    inventory list.

--- battlesystem.py
+++ battlesystem.py
@@ -1,14 +1,32 @@
 import random
 
+descriptors = 'funny sharp dull snakes powerful gleeming wobbly ugly golden rakish silver furry yucky slimy smelly rusty mysterious'.split()
+names = "thwacker poop bully whimpy destructor crumble notverygood".split()
+
 class character:
-	def __init__(self, name, hp, truth):
+	def __init__(self, name, hp, truth, inventory):
 		self.name = name
 		self.hp = hp
 		self.alive = truth
+		self.inventory = inventory
 
 	def check_alive(self):
 		if self.hp < 1:
 			self.alive = False
+
+	def add_inventory(self, new_sword):
+		self.inventory.append(new_sword)
+
+
+class sword:
+	def __init__(self, name, blade, hilt, magic):
+		self.name = name
+		self.blade = blade
+		self.hilt = hilt
+		self.magic = magic
+
+	def view(self):
+		print("The sword is called  " + self.name + ". Its blade is " + self.blade + ". Its hilt is " + self.hilt + ". Its magic is " + self.magic + ".")
 
 def engine(player_turn):
 	if player_turn == True:
@@ -42,18 +60,28 @@ def ai_command():
 		k = heal(ai_go)
 		return k
 
+def create_sword():
+	descriptor_index1 = random.randint(0, len(descriptors) - 1)
+	descriptor_index2 = random.randint(0, len(descriptors) - 1)
+	descriptor_index3 = random.randint(0, len(descriptors) - 1)
+	name_index = random.randint(0, len(names) - 1)
+	new_sword  = sword(names[name_index], descriptors[descriptor_index1], descriptors[descriptor_index2], descriptors[descriptor_index3])
+	return new_sword
+
+
 #these are my commands
 
 #receives input to determine which object to apply the damage toward
 def attack(ai_go = False):
-	d = random.randint(0, 6)
 	#player's turn
 	if ai_go == False:
+		d = random.randint(0, 6)
 		print("You attack for " + str(d) + " damage!")
 		game.hp = game.hp - d
 		return False
 	#game turn
 	else:
+		d = random.randint(0, 6)
 		print("you were attacked for " + str(d) + " damage!")
 		player.hp = player.hp - d
 		return True
@@ -90,8 +118,20 @@ def heal(ai_go = False):
 commands = {'attack' : attack, 'turtle' : turtle,
 	'commands' : list_commands, 'heal' : heal}
 
-player = character("you", 10, True)
-game = character("game", 10, True)
+
+#this is the game
+
+
+
+
+
+player = character("you", 10, True, [])
+game = character("game", 10, True, [])
+
+print("You pick up a sword.")
+new_sword = create_sword()
+new_sword.view()
+player.add_inventory(new_sword)
 
 print("You have encountered an enemy")
 
